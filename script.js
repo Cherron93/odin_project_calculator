6aa5d8a660c12e22b4b62c9da32445f00191bae5
Cherron93 <colinherron93@gmail.com>
2022-06-07 08:58:19 -0400

    Create display

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const numBtns = Array.from(document.getElementsByClassName('num'));
+const display = document.getElementById('display');
+
+console.log(btns)
+
+
+
 function add (a, b) {
     return a + b;
 };
@@ -17,5 +24,11 @@ function divide (a, b) {
 function operate (operator, a, b) {
     return operator(a, b);
 };
+
+numBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
+    })
+});
 
 
