cebcc65ec0d04668a39e93a8100b7a49aee7b525
Cherron93 <colinherron93@gmail.com>
2022-06-20 12:16:28 -0400

    Update equalsBtn function

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-const numBtns = Array.from(document.getElementsByClassName('btn num'));
 const operators = Array.from(document.getElementsByClassName('btn operator'))
 const display = document.getElementById('display');
 const equalsBtn = document.getElementById('equalsBtn');
-const btns = Array.from(document.getElementsByClassName('btn'));
 const valueBtns = Array.from(document.getElementsByClassName('btn value'))
 const clearBtn = document.getElementById('clear');
 
-function add (a, b) {
-    return a + b;
-};
-
-function subtract (a, b) {
-    return a - b;
-};
-
-function multiply (a, b) {
-    return a * b;
-};
-
-function divide (a, b) {
-    return a / b;
-};
-
-function operate (operator, a, b) {
-    return operator(a, b);
-};
-
-let displayValue = 0;
-let currentValue = 0;
-let calcArray = [];
+let currentDisplay = display.innerHTML;
 
 valueBtns.forEach(btn => {
     btn.addEventListener('click', function () {
-        display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
-        displayValue = display.innerHTML;
+        if (btn.innerHTML == '.') {
+            if (currentDisplay.includes('.')){
+                alert('Only one decimal allowed!')
+            } else {
+                display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
+            }
+        } else {
+            display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
+        }
     })
 });
 
+operators.forEach(btn => {
+    btn.addEventListener('click', function () {
+        display.innerHTML = `${display.innerHTML} ${btn.innerHTML} `
+    })
+})
 
-equalsBtn.addEventListener('click', () => display.innerHTML = eval(displayValue));
+equalsBtn.addEventListener('click', function () {
+    const displayArray = display.innerHTML.split(" ")
+    let currentValue = displayArray[0];
+    for (let i = 1; i <= displayArray.length; i ++) {
+        if (displayArray[i] == '+') {
+            currentValue = eval(`${currentValue} + ${displayArray[i + 1]}`);
+        } else if (displayArray[i] == '-') {
+            currentValue = eval(`${currentValue} - ${displayArray[i + 1]}`);
+        } else if (displayArray[i] == '/') {
+            if (displayArray[i + 1] == 0) {
+                alert("No dividing by zero!")
+                display.innerHTML = ''; 
+            } else {
+                currentValue = eval(`${currentValue} / ${displayArray[i + 1]}`);
+            }
+        } else if (displayArray[i] == '*') {
+            currentValue = eval(`${currentValue} * ${displayArray[i + 1]}`);
+        }
+    };
+    if (Number.isInteger(currentValue)) {
+        display.innerHTML = currentValue
+    } else {
+        display.innerHTML = currentValue.toFixed(2);
+    };
+});
 
 clearBtn.addEventListener('click', ()  => display.innerHTML = '');
-
-
