eb76949fff44fdad3794bb6da5868929e14653d1
Cherron93 <colinherron93@gmail.com>
2022-06-26 10:08:32 -0400

    Add keyboard functionality

--- script.js
+++ script.js
@@ -23,17 +23,40 @@ valueBtns.forEach(btn => {
     })
 });
 
+// function updateDisplay () {
+//     let btnText = display.innerHTML;
+//     if (btn.innerHTML == '.') {
+//         if (btnText.indexOf('.') > -1) {
+//             console.log(btn.innerHTML)
+//             alert('Only one decimal allowed!')
+//         } else {
+//         display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
+//         };
+//     } else {
+//         display.innerHTML = `${display.innerHTML}${btn.innerHTML}`;
+//     }
+// };
+
+// valueBtns.forEach(btn => {
+//     btn.addEventListener('click', () => updateDisplay());
+// });
+
 operators.forEach(btn => {
     btn.addEventListener('click', function () {
         display.innerHTML = `${display.innerHTML} ${btn.innerHTML} `
     })
 })
 
-equalsBtn.addEventListener('click', function () {
+
+
+function calculate () {
     const displayArray = display.innerHTML.split(" ")
-    let currentValue = displayArray[0];
+    console.log(displayArray)
+    let currentValue = parseInt(displayArray[0]);
     for (let i = 1; i <= displayArray.length; i ++) {
         if (displayArray[i] == '+') {
+            console.log(currentValue);
+            console.log(displayArray[i + 1])
             currentValue = eval(`${currentValue} + ${displayArray[i + 1]}`);
         } else if (displayArray[i] == '-') {
             currentValue = eval(`${currentValue} - ${displayArray[i + 1]}`);
@@ -49,15 +72,62 @@ equalsBtn.addEventListener('click', function () {
         }
     };
     if (Number.isInteger(currentValue)) {
-        display.innerHTML = currentValue
+        display.innerHTML = currentValue;
     } else {
         display.innerHTML = currentValue.toFixed(2);
     };
-});
+}
+
+equalsBtn.addEventListener('click', () => calculate()); 
+
+// equalsBtn.addEventListener('click', () => evaluate()); {
+//     // const displayArray = display.innerHTML.split(" ")
+//     // let currentValue = displayArray[0];
+//     // for (let i = 1; i <= displayArray.length; i ++) {
+//     //     if (displayArray[i] == '+') {
+//     //         currentValue = eval(`${currentValue} + ${displayArray[i + 1]}`);
+//     //     } else if (displayArray[i] == '-') {
+//     //         currentValue = eval(`${currentValue} - ${displayArray[i + 1]}`);
+//     //     } else if (displayArray[i] == '/') {
+//     //         if (displayArray[i + 1] == 0) {
+//     //             alert("No dividing by zero!")
+//     //             display.innerHTML = ''; 
+//     //         } else {
+//     //             currentValue = eval(`${currentValue} / ${displayArray[i + 1]}`);
+//     //         }
+//     //     } else if (displayArray[i] == '*') {
+//     //         currentValue = eval(`${currentValue} * ${displayArray[i + 1]}`);
+//     //     }
+//     // };
+//     // if (Number.isInteger(currentValue)) {
+//     //     display.innerHTML = currentValue
+//     // } else {
+//     //     display.innerHTML = currentValue.toFixed(2);
+//     // };
+// });
 
 clearBtn.addEventListener('click', ()  => display.innerHTML = '');
 
 backSpaceBtn.addEventListener('click', () => {
     let displayText = display.innerHTML;
     display.innerHTML = displayText.slice(0, -1)
+});
+
+document.addEventListener('keypress', (e) => {
+    if (e.key == 'Enter') {
+        calculate();
+    } else if (e.key == '0' ||e.key == '1' || e.key == '2' || e.key == '3' || e.key == '4' || e.key == '5' || e.key == '6' || e.key == '7' || e.key == '8' || e.key == '9' || e.key == '(' || e.key == ')' || e.key == '.') {
+        let btnText = display.innerHTML;
+        if (e.key == '.') {
+            if (btnText.indexOf('.') > -1) {
+                alert('Only one decimal allowed!')
+            } else {
+            display.innerHTML = `${display.innerHTML}${e.key}`;
+            };
+        } else {
+            display.innerHTML = `${display.innerHTML}${e.key}`;
+        }
+    } else if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
+        display.innerHTML = `${display.innerHTML} ${e.key} `
+    };
 });
